dfff5005f7ba6088fd02a75eb6f8d79cabd53681
dejosemario <josemariaodumodu@gmail.com>
2026-07-21 00:00:26 +0100

    feat: implment hugging face embedding function

--- embeddings.py
+++ embeddings.py
@@ -1,7 +1,33 @@
-# 1. import
-# 2. load the env variables
-# 3. build the hugging face api url
-# 4. define a function called get-embedding(which gets the chunk text as property)
-#   - takes a chunk  of text
-#   - sends it to the hugging face api
-#   - returns the vector list (list of numbers)
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HF_API_KEY = os.getenv("HF_API_KEY")
+EMBED_MODEL_NAME = os.getenv(
+    "EMBED_MODEL_NAME", "sentence-transformers/all-MiniLM-L6-v2"
+)
+
+
+API_URL = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{EMBED_MODEL_NAME}"
+
+headers = {"Authorization": f"Bearer {HF_API_KEY}"}
+
+def get_embedding(text: str) -> list[float]:
+
+    response = requests.post(
+        API_URL,
+        headers=headers,
+        json={"inputs": text, "options": {"wait_for_model": True}}
+    )
+    
+    if response.status_code != 200:
+        raise Exception(f"HuggingFace API error: {response.text}")
+    
+    result = response.json()
+    
+    if isinstance(result, list) and isinstance(result[0], list):
+        return result[0]
+    
+    return result
