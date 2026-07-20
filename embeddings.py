0e1058e8b086d0a37992c5ecd9eef70707f814ef
dejosemario <josemariaodumodu@gmail.com>
2026-07-20 23:36:09 +0100

    chore: add embeddings module pseudocode and structure

---
+++ embeddings.py
@@ -0,0 +1,7 @@
+# 1. import
+# 2. load the env variables
+# 3. build the hugging face api url
+# 4. define a function called get-embedding(which gets the chunk text as property)
+#   - takes a chunk  of text
+#   - sends it to the hugging face api
+#   - returns the vector list (list of numbers)
