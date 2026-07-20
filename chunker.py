3baa8c0ef65a058222e992d5b096f27bacb083e8
dejosemario <josemariaodumodu@gmail.com>
2026-07-20 23:21:01 +0100

    feat: implement semantic chunker with configurable chunker length

--- chunker.py
+++ chunker.py
@@ -6,7 +6,7 @@ load_dotenv()
 nltk.download('punkt', quiet=True)
 nltk.download('punkt_tab', quiet=True)
 
-CHUNK_LENGTH = int(os.getenv("CHUNK_LENGT", 200))
+CHUNK_LENGTH = int(os.getenv("CHUNK_LENGTH", 200))
 
 def chunk_text(text: str) -> list[str]:
     sentences = nltk.sent_tokenize(text)
@@ -15,11 +15,18 @@ def chunk_text(text: str) -> list[str]:
     current_length = 0 
     
     for sentence in sentences:
-        #get the word count length with var word_count
         word_count = len(sentence.split())
         
-        #loop through and check if the curreent_lenght plus the word count is greater than or equal to the chunk_lenght and current_chunk, if not
         if current_length + word_count > CHUNK_LENGTH and current_chunk:
-            #if so, then appened the chunk with the current chunk and set everyhing bak to 0
-            chunks.append
+            chunks.append(" ".join(current_chunk))
+            current_chunk = []
+            current_length = 0 
+        current_chunk.append(sentence)
+        current_length += word_count
+        
+    if current_chunk: 
+        chunks.append(" ".join(current_chunk))
+        
+    return chunks
+        
             
